603a1238b97ee73e0894b248ff3d7bd517af1501
andreascas <andreas@ecaspar.dk>
2019-04-29 23:36:04 +0200

    Update Overhead2.py

--- Overhead2.py
+++ Overhead2.py
@@ -3,39 +3,55 @@ import os
 import re
 #Definer fil listen som vil indeholde alle Pcap filer
 File_list = []
+Flow_Ids = []
 def Flow_monitor():
-    # Vi laeser foesrt indholdet af XML filen
+    #Vi laeser foesrt indholdet af XML filen
     f= open('Aodvtest.xml', 'r')
     f1 = f.readlines()
     f.close()
-    #vi gemmer dog kun linje 3 som indeholder det relevante data
-    Data_line = f1[3]
+    substring = "timeFirstTxPacket"
+    for lines in f1:
+        if substring in lines:
+            Flow_Ids.append(lines)
+    DataSent = 0
+    DataReceived = 0
+    PacketsSent = 0
+    PacketsReceived = 0
+    PacketsDropped = 0
+    Unlisted_Dropped = 0
+    EndtoEndDelay = 0
+    # We will go through each individual Flow_ID and find their respective data and sum it up
+    for lines in Flow_Ids:
     #Vi splitter nu linjen op saa vi kun faar strings mellem 2 " "
-    Data = re.split('"|"',Data_line)
+        Data_line = lines
+        Data = re.split('"|"',Data_line)
     #Vi gemmer saa de nye substrings som er relevante
-    # Disse er gennemgaaet manuelt og navnet svarer til talvaerdien set i xml filen uden +/- og ns
-    FirstPacketSent = int(Data[3][1:-4])
-    FirstPacketReceived = int(Data[5][1:-4]) - FirstPacketSent
-    LastPacketSent = int(Data[7][1:-4]) - FirstPacketSent
-    LastPacketReceived = int(Data[9][1:-4]) - FirstPacketSent
-    Delaysum = int(Data[11][1:-4])
-    Jittersum = int(Data[13][1:-4])
-    DataSent = int(Data[17])
-    DataReceived = int(Data[19])
-    PacketsSent = int(Data[21])
-    PacketsReceived = int(Data[23])
-    PacketsDropped =  int(Data[25])
-    #AverageDelay = ,Delaysum / PacketsSent
-    AverageDelay = Delaysum / PacketsSent
-    OverheadData = int(SamletDataSendt()) - DataSent 
+    #Disse er gennemgaaet manuelt og navnet svarer til talvaerdien set i xml filen uden +/- og ns
+        #FirstPacketSent =  int(Data[3][1:-4])
+        #FirstPacketReceived = int(Data[5][1:-4]) - FirstPacketSent
+        #LastPacketSent = int(Data[7][1:-4]) - FirstPacketSent
+        #LastPacketReceived = int(Data[9][1:-4]) - FirstPacketSent
+        #Delaysum = int(Data[11][1:-4])
+        #Jittersum = int(Data[13][1:-4])
+        PacketsSent = int(Data[21]) + PacketsSent
+        PacketsReceived = int(Data[23]) + PacketsReceived
+        PacketsDropped =  int(Data[25]) + PacketsDropped
+        DataSent = 1000 * PacketsSent + DataSent
+        DataReceived = 1000 * PacketsReceived + DataReceived
+        tempPacketsReceived = int(Data[23])
+        if tempPacketsReceived != 0:
+            EndtoEndDelay = EndtoEndDelay + (int(Data[11][1:-4]) /tempPacketsReceived)
+            
+    # We calculate data based on the sum of the previous extracted data 
+    OverheadData = int(SamletDataSendt()) - DataSent
+    Average_EndtoEndDelay = EndtoEndDelay / len(Flow_Ids)
+    Unlisted_Dropped = (PacketsSent - (PacketsReceived + PacketsDropped))
     f= open('Collected_data.txt', 'a')
-    f.write('First packet received at %d ns from start \n' % (FirstPacketReceived))
-    f.write ('Average delay is %d ns \nTotal amount of packets sent %d\n Toal amount of packets received %d\n' %(AverageDelay, PacketsSent, PacketsReceived))
+    #f.write('First packet received at %d ns from start \n' % (FirstPacketReceived))
+    f.write ('Total amount of packets sent %d\n Toal amount of packets received %d\nThe Average end to end delay was %d nanoseconds\n' %( PacketsSent, PacketsReceived, Average_EndtoEndDelay))
     f.write ('Packets lost in transmission %d\nA total of %d bytes were sent\nA total of %d bytes were received\n' %(PacketsDropped, DataSent, DataReceived))
-    f.write ('Overhead Data sent is %d\n' % OverheadData)
+    f.write ('Overhead Data sent is %d\nA total of %d were dropped but not listed\n' % (OverheadData, Unlisted_Dropped))
     f.close
-
-
 def SamletDataSendt():
     
  #Vi finder alle Pcap filer i den folderen vi er i

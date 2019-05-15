86d94f7717ebaca45fe67b714f2bf4062c0ee7ed
JellyBelly <36983371+JellyBelly3D@users.noreply.github.com>
2019-05-15 11:31:53 +0200

    fixed run comand

--- nodecounttest.sh
+++ nodecounttest.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # full path to the ns3 directory
-ns3_path="/home/mikkel/ns3workspace/ns-3-allinone/ns-3-dev"
+ns3_path="/home/jellybelly/ns-allinone-3.29/ns-3.29/"
 
 # Runs per configuration
-runs=10
+runs=1
 # Protocols to run tests on
 protocol="olsr"
 
 # For testing different node counts
 max_nodes=50
 min_nodes=10
-node_step=10
+node_step=5
 
 cd "$ns3_path"
 pwd
@@ -26,7 +26,8 @@ for (( nodes=$min_nodes; nodes<=$max_nodes; nodes+=$node_step)); do
     for (( run=0; run<$runs; run++ )); do
         echo "Starting $protocol-$run-$nodes-n test"
         mkdir -p "./runs/$protocol/n$nodes/run$run"
-        ./waf --cwd="./runs/$protocol/run$run" --run "mainp6 --runNumber=$run --numNodes=$nodes" &
+        ./waf --cwd="./runs/$protocol/n$nodes/run$run" --run "mainp6 --runNumber=$run --numNodes=$nodes" &
     done
 done
      
+exit 0
